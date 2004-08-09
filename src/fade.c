ad4728fecc7f702b0a2aa457a15226d4a42b08ce
Dave Murphy <wntrmute@users.sourceforge.net>
2004-08-09 17:04:51 +0000

    project license added to source files

--- src/fade.c
+++ src/fade.c
@@ -1,11 +1,39 @@
-//---------------------------------------------------------------------------------
-// GBA 256 color fade routines
-// Fade to black implemented as a special case
-// Calling FadeToPalette with a buffer of all zeros will do the same thing
-//
-// FadeToPalette will also perform a cross fade effect
-//
-//---------------------------------------------------------------------------------
+/*
+	"$Id: fade.c,v 1.2 2004-08-09 17:04:51 wntrmute Exp $"
+
+	libgba 256 color fade routines
+
+	Copyright 2003-2004 by Dave Murphy.
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Library General Public
+	License as published by the Free Software Foundation; either
+	version 2 of the License, or (at your option) any later version.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Library General Public License for more details.
+
+	You should have received a copy of the GNU Library General Public
+	License along with this library; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+	USA.
+
+	Please report all bugs and problems through the bug tracker at
+	"http://sourceforge.net/tracker/?group_id=114505&atid=668551".
+
+	"$Header: /lvm/shared/ds/ds/cvs/devkitpro-cvsbackup/libgba/src/fade.c,v 1.2 2004-08-09 17:04:51 wntrmute Exp $"
+
+*/
+
+/*---------------------------------------------------------------------------------
+	Fade to black implemented as a special case
+	Calling FadeToPalette with a buffer of all zeros will do the same thing
+
+	FadeToPalette will also perform a cross fade effect
+
+---------------------------------------------------------------------------------*/
 #include "gba_video.h"
 #include "gba_interrupt.h"
 #include "gba_systemcalls.h"
--- src/xcomms.c
+++ src/xcomms.c
@@ -1,3 +1,31 @@
+/*
+	"$Id: xcomms.c,v 1.2 2004-08-09 17:04:51 wntrmute Exp $"
+
+	libgba Xboo Communicator support routines
+
+	Copyright 2003-2004 by Dave Murphy.
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Library General Public
+	License as published by the Free Software Foundation; either
+	version 2 of the License, or (at your option) any later version.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Library General Public License for more details.
+
+	You should have received a copy of the GNU Library General Public
+	License along with this library; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+	USA.
+
+	Please report all bugs and problems through the bug tracker at
+	"http://sourceforge.net/tracker/?group_id=114505&atid=668551".
+
+	"$Header: /lvm/shared/ds/ds/cvs/devkitpro-cvsbackup/libgba/src/xcomms.c,v 1.2 2004-08-09 17:04:51 wntrmute Exp $"
+
+*/
 #include <string.h>
 #include "gba_sio.h"
 #include "gba_video.h"
@@ -22,7 +50,6 @@ u32 xcomms_recv(void)
 void xcomms_send(u32 data)
 //---------------------------------------------------------------------------------
 {
-//	REG_SIOCNT	=	SIO_32BIT | SIO_NOTRDY;
 	REG_SIODATA32 = data;
 	REG_SIOCNT	=	SIO_32BIT;
 	REG_SIOCNT	=	SIO_32BIT | SIO_START;
@@ -43,7 +70,6 @@ void xcomms_init()
 u32 xcomms_exchange(u32 data)
 //---------------------------------------------------------------------------------
 {
-//	REG_SIOCNT	=	SIO_32BIT | SIO_NOTRDY;
 	REG_SIODATA32 = data;
 	REG_SIOCNT	=	SIO_32BIT;
 	REG_SIOCNT	=	SIO_32BIT | SIO_START;

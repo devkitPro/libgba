80d9e70d2ebfaf5bebcc37f16b9e4925f00ed69f
Dave Murphy <wntrmute@users.sourceforge.net>
2005-09-12 04:45:43 +0000

    added *printf functionality

--- Makefile
+++ Makefile
@@ -11,15 +11,18 @@ endif
 
 BUILD		:=	build
 SOURCES		:=	src src/BoyScout
-INCLUDES	:=	include
+INCLUDES	:=	include build
+DATA		:=	data
 
 DATESTRING	:=	$(shell date +%Y)$(shell date +%m)$(shell date +%d)
+
+ARCH	:=	-mthumb -mthumb-interwork
 
 #---------------------------------------------------------------------------------
 # options for code generation
 #---------------------------------------------------------------------------------
-CFLAGS	:=	-g -O3 -Wall -Wno-switch -Wno-multichar -mthumb -mthumb-interwork $(INCLUDE)
-ASFLAGS	:=	-g -Wa,--warn -mthumb -mthumb-interwork
+CFLAGS	:=	-g -O3 -Wall -Wno-switch -Wno-multichar $(ARCH) $(INCLUDE)
+ASFLAGS	:=	-g -Wa,--warn $(ARCH)
 
 #---------------------------------------------------------------------------------
 # path to tools - this can be deleted if you set the path in windows
@@ -32,20 +35,21 @@ ifneq ($(BUILD),$(notdir $(CURDIR)))
 
 export TARGET	:=	$(CURDIR)/lib/libgba.a
 
-export VPATH	:=	$(foreach dir,$(SOURCES),$(CURDIR)/$(dir))
+export VPATH	:=	$(foreach dir,$(DATA),$(CURDIR)/$(dir)) $(foreach dir,$(SOURCES),$(CURDIR)/$(dir))
 
 PREFIX	:=	arm-elf-
 
-export CC		:=	$(PREFIX)gcc
-export AS		:=	$(PREFIX)as
-export LD		:=	$(PREFIX)gcc
-export AR		:=	$(PREFIX)ar
+export CC	:=	$(PREFIX)gcc
+export AS	:=	$(PREFIX)as
+export LD	:=	$(PREFIX)gcc
+export AR	:=	$(PREFIX)ar
 export OBJCOPY	:=	$(PREFIX)objcopy
 
-CFILES	:=	$(foreach dir,$(SOURCES),$(notdir $(wildcard $(dir)/*.c)))
-SFILES	:=	$(foreach dir,$(SOURCES),$(notdir $(wildcard $(dir)/*.s)))
+CFILES		:=	$(foreach dir,$(SOURCES),$(notdir $(wildcard $(dir)/*.c)))
+SFILES		:=	$(foreach dir,$(SOURCES),$(notdir $(wildcard $(dir)/*.s)))
+BINFILES	:=	$(foreach dir,$(DATA),$(notdir $(wildcard $(dir)/*.*)))
 
-export OFILES	:=	$(CFILES:.c=.o) $(SFILES:.s=.o)
+export OFILES	:=	$(addsuffix .o,$(BINFILES)) $(CFILES:.c=.o) $(SFILES:.s=.o)
 export INCLUDE	:=	$(foreach dir,$(INCLUDES),-I$(CURDIR)/$(dir))
 export DEPSDIR	:=	$(CURDIR)/build
 
@@ -62,7 +66,7 @@ clean:
 	@rm -fr $(BUILD) *.tar.bz2
 
 dist: $(BUILD)
-	@tar --exclude=*CVS* --exclude=*build* --exclude=*.bz2 -cvjf libgba-src-$(DATESTRING).tar.bz2 *
+	@tar --exclude=*CVS* --exclude=*build* --exclude=*.bz2 -cvjf libgba-src-$(DATESTRING).tar.bz2 include src data Makefile license.txt
 	@tar --exclude=*CVS* -cvjf libgba-$(DATESTRING).tar.bz2 include lib license.txt
 
 install: dist
@@ -83,6 +87,11 @@ $(TARGET): $(OFILES)
 	@echo $@
 	@rm -f $@
 	@$(AR) rcs $@ $(OFILES)
+
+%.fnt.o	:	%.fnt
+#---------------------------------------------------------------------------------
+	@echo $(notdir $<)
+	@$(bin2o)
 
 -include $(DEPENDS)
 
---
+++ src/console.c
@@ -0,0 +1,284 @@
+/*
+	$Id: console.c,v 1.1 2005-09-12 04:45:43 wntrmute Exp $
+
+	Copyright 2003-2004 by Dave Murphy.
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Library General Public
+	License as published by the Free Software Foundation; either
+	version 2 of the License, or (at your option) any later version.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Library General Public License for more details.
+
+	You should have received a copy of the GNU Library General Public
+	License along with this library; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+	USA.
+
+	Please report all bugs and problems through the bug tracker at
+	"http://sourceforge.net/tracker/?group_id=114505&atid=668551".
+
+	$Log: not supported by cvs2svn $
+
+*/
+
+#include <sys/iosupport.h>
+#include <gba_video.h>
+#include <gba_systemcalls.h>
+#include <gba_console.h>
+
+#include <stdio.h>
+
+void consolePrintChar(char c);
+
+//---------------------------------------------------------------------------------
+int con_read(struct _reent *r,int fd,char *ptr,int len) {
+//---------------------------------------------------------------------------------
+	return -1;
+}
+
+//---------------------------------------------------------------------------------
+int con_close(struct _reent *r,int fd) {
+//---------------------------------------------------------------------------------
+	return -1;
+}
+
+static bool consoleInitialised = false;
+
+//---------------------------------------------------------------------------------
+int con_open(struct _reent *r,const char *path,int flags,int mode) {
+//---------------------------------------------------------------------------------
+	if (consoleInitialised) return 0;
+	return -1;
+}
+/*
+       ESC code sequence                  Function
+       -------------------         ---------------------------
+Cursor Controls:
+         ESC[#;#H or ESC[#;#f      Moves cusor to line #, column #
+         ESC[#A                    Moves cursor up # lines
+         ESC[#B                    Moves cursor down # lines
+         ESC[#C                    Moves cursor forward # spaces
+         ESC[#D                    Moves cursor back # spaces
+         ESC[s                     Saves cursor position for recall later
+         ESC[u                     Return to saved cursor position
+
+Erase Functions:
+         ESC[2J                    Clear screen and home cursor
+         ESC[K                     Clear to end of line
+
+Set Graphics Rendition:
+         ESC[#;#;....;#m           not implemented
+*/
+
+
+static int consoleX, consoleY;
+static int savedX, savedY;
+static int consoleMap, consolePalette;
+
+static const int CONSOLE_WIDTH=30;
+static const int CONSOLE_HEIGHT=20;
+
+//---------------------------------------------------------------------------------
+void consoleCls() {
+//---------------------------------------------------------------------------------
+
+	*((u32 *)MAP_BASE_ADR(consoleMap)) =0;
+	CpuFastSet( MAP_BASE_ADR(consoleMap), MAP_BASE_ADR(consoleMap), FILL | (0x800/4));
+}
+
+//---------------------------------------------------------------------------------
+int con_write(struct _reent *r,int fd,const char *ptr,int len) {
+//---------------------------------------------------------------------------------
+	char chr;
+
+	if (!consoleInitialised) return -1;
+
+	int i, count = 0;
+	char *tmp = (char*)ptr;
+
+	if(!tmp || len<=0) return -1;
+
+	i = 0;
+	while(*tmp!='\0' && i<len) {
+
+		chr = *(tmp++);
+		count++;
+
+		if ( chr == 0x1b && *tmp == '[' ) {
+			bool escaping = true;
+			char *escapeseq	= tmp;
+			int escapelen = 0;
+
+			do {
+				chr = *(tmp++);
+				i++; count++; escapelen++;
+				int parameter;
+				switch (chr) {
+					case 'H':
+						sscanf(escapeseq,"[%d;%dH", &consoleY, &consoleX);
+						escaping = false;
+						break;
+					case 'f':
+						sscanf(escapeseq,"[%d;%df", &consoleY, &consoleX);
+						escaping = false;
+						break;
+					case 'A':
+						sscanf(escapeseq,"[%dA", &parameter);
+						consoleY =  (consoleY - parameter) < 0 ? 0 : consoleY - parameter;
+						escaping = false;
+						break;
+					case 'B':
+						sscanf(escapeseq,"[%dB", &parameter);
+						consoleY =  (consoleY + parameter) > CONSOLE_HEIGHT - 1 ? CONSOLE_HEIGHT - 1 : consoleY + parameter;
+						escaping = false;
+						break;
+					case 'C':
+						sscanf(escapeseq,"[%dC", &parameter);
+						consoleX =  (consoleX + parameter) > CONSOLE_WIDTH - 1 ? CONSOLE_WIDTH - 1 : consoleX + parameter;
+						escaping = false;
+						break;
+					case 'D':
+						sscanf(escapeseq,"[%dC", &parameter);
+						consoleX =  (consoleX - parameter) < 0 ? 0 : consoleX - parameter;
+						escaping = false;
+						break;
+					case 'K':
+						escaping = false;
+						break;
+					case 's':
+						savedX = consoleX;
+						savedY = consoleY;
+						escaping = false;
+						break;
+					case 'u':
+						consoleX = savedX;
+						consoleY = savedY;
+						escaping = false;
+						break;
+					case 'J':
+						if ( escapeseq[escapelen-2] == '2') {
+							consoleCls();
+							consoleX = 0;
+							consoleY = 0;
+						}
+						escaping = false;
+					break;
+				}
+			} while (escaping);
+		continue;
+		}
+
+		consolePrintChar(chr);
+		i++;
+	}
+
+	return count;
+}
+
+
+const devoptab_t dotab_stdout = {"con",con_open,con_close,con_write,con_read,NULL,NULL};
+const devoptab_t dotab_stderr = {"con",con_open,con_close,con_write,con_read,NULL,NULL};
+
+
+//---------------------------------------------------------------------------------
+// upcvt_4bit()
+// Convert a 1-bit font to GBA 4-bit format.
+//---------------------------------------------------------------------------------
+void upcvt_4bit(void *dst, const u8 *src, u32 len) {
+//---------------------------------------------------------------------------------
+	u32 *out = dst;
+
+	for(; len > 0; len--) {
+		u32 dst_bits = 0;
+		u32 src_bits = *src++;
+		u32 x;
+
+		for(x = 0; x < 8; x++) {
+			dst_bits |= src_bits & 1;
+			dst_bits <<= 4;
+			src_bits >>= 1;
+		}
+	*out++ = dst_bits;
+	}
+}
+
+#include <font8x8_fnt.h>
+
+//---------------------------------------------------------------------------------
+void consoleInit(	int charBase, int mapBase, int background,
+					const u8* font, int fontsize, int palette) {
+//---------------------------------------------------------------------------------
+
+	BGCTRL[background] = BG_SIZE_0 | CHAR_BASE(charBase) | SCREEN_BASE(mapBase);
+
+	if (font == NULL || fontsize == 0) {
+		font = font8x8_fnt;
+		fontsize = font8x8_fnt_size;
+	}
+
+	upcvt_4bit(CHAR_BASE_ADR(charBase), font, fontsize);
+
+	consoleMap = mapBase;
+	consolePalette = palette;
+
+	devoptab_list[STD_OUT] = &dotab_stdout;
+	devoptab_list[STD_ERR] = &dotab_stderr;
+	setvbuf(stderr, NULL , _IONBF, 0);
+	setvbuf(stdout, NULL , _IONBF, 0);
+
+	consoleInitialised = true;
+
+}
+
+//---------------------------------------------------------------------------------
+static void newRow() {
+//---------------------------------------------------------------------------------
+	consoleY++;
+
+	if(consoleY >= CONSOLE_HEIGHT) {
+		consoleY--;
+
+		u16 * dst = (u16 *)(VRAM + (SCREEN_BASE(consoleMap)<<3));
+		u16 * src = (u16 *)(VRAM + (SCREEN_BASE(consoleMap)<<3) + (1 << 6));
+
+		int i;
+		for ( i = 0; i < CONSOLE_HEIGHT; i++ ) {
+			CpuFastSet( src, dst, COPY32 | (CONSOLE_WIDTH/2) );
+			dst += (1<<5);
+			src += (1<<5);
+		}
+	}
+}
+
+//---------------------------------------------------------------------------------
+void consolePrintChar(char c) {
+//---------------------------------------------------------------------------------
+
+
+	u16*	ScreenBase = (u16 *)(VRAM + (SCREEN_BASE(consoleMap)<<3) + (consoleX<<1) + (consoleY<<6) );
+
+	if(consoleX >= CONSOLE_WIDTH) {
+		consoleX = 0;
+		newRow();
+	}
+
+
+	switch(c) {
+
+		case 10:
+		case 11:
+		case 12:
+		case 13:
+			newRow();
+			consoleX = 0;
+			break;
+		default:
+			*(ScreenBase) = BG_PALETTE(consolePalette) | (u8)c;
+		consoleX++;
+
+	}
+}

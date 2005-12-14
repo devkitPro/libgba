e449a4ee4c48678b289ef1c1ccb02592f18ae8a0
Dave Murphy <wntrmute@users.sourceforge.net>
2005-12-14 14:24:42 +0000

    corrected MULTIBOOT_MODES enum

--- include/gba_multiboot.h
+++ include/gba_multiboot.h
@@ -1,5 +1,5 @@
 /*
-	"$Id: gba_multiboot.h,v 1.2 2004-08-08 19:28:07 wntrmute Exp $"
+	"$Id: gba_multiboot.h,v 1.3 2005-12-14 14:24:42 wntrmute Exp $"
 
 	Header file for libgba multiboot definitions
 
@@ -23,7 +23,7 @@
 	Please report all bugs and problems through the bug tracker at
 	"http://sourceforge.net/tracker/?group_id=114505&atid=668551".
 
-	"$Header: /lvm/shared/ds/ds/cvs/devkitpro-cvsbackup/libgba/include/gba_multiboot.h,v 1.2 2004-08-08 19:28:07 wntrmute Exp $"
+	"$Header: /lvm/shared/ds/ds/cvs/devkitpro-cvsbackup/libgba/include/gba_multiboot.h,v 1.3 2005-12-14 14:24:42 wntrmute Exp $"
 
 */
 
@@ -60,7 +60,7 @@ typedef	struct {
 	u8	server_type;
 } MultiBootParam;
 
-enum { MODE32_NORMAL, MODE16_MULTI, MODE32_2MHZ);
+enum MULTIBOOT_MODES { MODE32_NORMAL, MODE16_MULTI, MODE32_2MHZ};
 
 u32 MultiBoot(MultiBootParam *mp, u32 mode);
 

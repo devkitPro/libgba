f6238b6d130b3f0c3c4748a60f60f41742ebc765
Dave Murphy <wntrmute@users.sourceforge.net>
2007-08-11 06:03:07 +0000

    make nesting really work

--- src/InterruptDispatcher.s
+++ src/InterruptDispatcher.s
@@ -1,9 +1,9 @@
 /*
-	$Id: InterruptDispatcher.s,v 1.9 2007-02-08 03:14:25 wntrmute Exp $
+	$Id: InterruptDispatcher.s,v 1.10 2007-08-11 06:03:07 wntrmute Exp $
 
 	libgba interrupt dispatcher routines
 
-	Copyright 2003-2005 by Dave Murphy.
+	Copyright 2003-2007 by Dave Murphy.
 
 	This library is free software; you can redistribute it and/or
 	modify it under the terms of the GNU Library General Public
@@ -23,29 +23,6 @@
 	Please report all bugs and problems through the bug tracker at
 	"http://sourceforge.net/tracker/?group_id=114505&atid=668551".
 
-	$Header: /lvm/shared/ds/ds/cvs/devkitpro-cvsbackup/libgba/src/InterruptDispatcher.s,v 1.9 2007-02-08 03:14:25 wntrmute Exp $
-
-	$Log: not supported by cvs2svn $
-	Revision 1.8  2007/02/07 17:08:44  wntrmute
-	acknowlege irq before handler
-	
-	Revision 1.7  2005/12/14 14:12:17  wntrmute
-	only save lr when handler executed
-	
-	Revision 1.6  2005/11/29 17:00:06  wntrmute
-	disable interrupts on return from handler
-	
-	Revision 1.5  2005/10/26 09:19:16  wntrmute
-	restore REG_IME on no handler
-	
-	Revision 1.4  2005/09/27 07:58:45  wntrmute
-	safer interrupt nesting
-	
-	Revision 1.3  2005/09/07 23:09:56  wntrmute
-	*** empty log message ***
-	
-	Revision 1.2  2005/08/31 23:17:30  wntrmute
-	corrected load of REG_IME
 
 */
 
@@ -64,7 +41,7 @@ IntrMain:
 	ldr	r1, [r3, #0x208]	@ r1 = IME
 	str	r3, [r3, #0x208]	@ disable IME
 	mrs	r0, spsr
-	stmfd	sp!, {r0-r1,r3}		@ {spsr, IME, REG_BASE}
+	stmfd	sp!, {r0-r1,r3,lr}	@ {spsr, IME, REG_BASE, lr_irq}
 
 	and	r1, r2,	r2, lsr #16	@ r1 =	IE & IF
 
@@ -90,7 +67,7 @@ findIRQ:
 no_handler:
 @---------------------------------------------------------------------------------
 	strh	r1, [r3, #0x02]		@ IF Clear
-	ldmfd	sp!, {r0-r1,r3}		@ {spsr, IME, REG_BASE}
+	ldmfd	sp!, {r0-r1,r3,lr}	@ {spsr, IME, REG_BASE, lr_irq}
 	str	r1, [r3, #0x208]	@ restore REG_IME
 	mov	pc,lr
 
@@ -128,7 +105,7 @@ IntrRet:
 	orr	r3, r3, #0x92		@ /  --> Disable IRQ. Enable FIQ. Set CPU mode to IRQ.
 	msr	cpsr, r3
 
-	ldmfd   sp!, {r0-r1,r3}		@ {spsr, IME, REG_BASE}
+	ldmfd   sp!, {r0-r1,r3,lr}	@ {spsr, IME, REG_BASE, lr_irq}
 	str	r1, [r3, #0x208]	@ restore REG_IME
 	msr	spsr, r0		@ restore spsr
 	mov	pc,lr

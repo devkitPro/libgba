7d5922c90e2ea0d6b246b5ee7a319ea080ff3089
Dave Murphy <davem@devkitpro.org>
2014-12-21 11:50:35 +0000

    use stdint/stdbool for types

--- include/gba_types.h
+++ include/gba_types.h
@@ -35,28 +35,24 @@
 #define	_gba_types_h_
 //---------------------------------------------------------------------------------
 
+#include <stdint.h>
+#include <stdbool.h>
+
 //---------------------------------------------------------------------------------
 // Data	types
 //---------------------------------------------------------------------------------
-typedef	unsigned char			u8;		/**< Unsigned 8 bit value	*/
-typedef	unsigned short int		u16;	/**< Unsigned 16 bit value	*/
-typedef	unsigned int			u32;	/**< Unsigned 32 bit value	*/
-typedef	signed char				s8;		/**< Signed 8 bit value	*/
-typedef	signed short int		s16;	/**< Signed 16 bit value	*/
-typedef	signed int				s32;	/**< Signed 32 bit value	*/
-typedef	volatile u8				vu8;	/**< volatile Unsigned 8 bit value	*/
-typedef	volatile u16			vu16;	/**< volatile Unigned 16 bit value	*/
-typedef	volatile u32			vu32;	/**< volatile Unsigned 32 bit value	*/
-typedef	volatile s8				vs8;	/**< volatile Signed 8 bit value	*/
-typedef	volatile s16			vs16;	/**< volatile Signed 8 bit value	*/
-typedef	volatile s32			vs32;	/**< volatile Signed 8 bit value	*/
-
-#ifndef __cplusplus
-/*! C++ compatible bool for C
-
-*/
-typedef enum { false, true } bool;
-#endif
+typedef	uint8_t			u8;	/**< Unsigned 8 bit value		*/
+typedef	uint16_t		u16;	/**< Unsigned 16 bit value		*/
+typedef	uint32_t		u32;	/**< Unsigned 32 bit value		*/
+typedef	int8_t			s8;	/**< Signed 8 bit value			*/
+typedef	int16_t			s16;	/**< Signed 16 bit value		*/
+typedef	int32_t			s32;	/**< Signed 32 bit value		*/
+typedef	volatile u8		vu8;	/**< volatile Unsigned 8 bit value	*/
+typedef	volatile u16		vu16;	/**< volatile Unigned 16 bit value	*/
+typedef	volatile u32		vu32;	/**< volatile Unsigned 32 bit value	*/
+typedef	volatile s8		vs8;	/**< volatile Signed 8 bit value	*/
+typedef	volatile s16		vs16;	/**< volatile Signed 8 bit value	*/
+typedef	volatile s32		vs32;	/**< volatile Signed 8 bit value	*/
 
 //---------------------------------------------------------------------------------
 #endif // _gba_types_h_

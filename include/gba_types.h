eb230a82e6a7e31c1bf555dabc9aa5d52b6da976
Dave Murphy <davem@devkitpro.org>
2015-01-06 16:45:23 +0000

    update for latest tools

--- include/gba_types.h
+++ include/gba_types.h
@@ -37,22 +37,22 @@
 
 #include <stdint.h>
 #include <stdbool.h>
-
 //---------------------------------------------------------------------------------
 // Data	types
 //---------------------------------------------------------------------------------
-typedef	uint8_t			u8;	/**< Unsigned 8 bit value		*/
-typedef	uint16_t		u16;	/**< Unsigned 16 bit value		*/
-typedef	uint32_t		u32;	/**< Unsigned 32 bit value		*/
-typedef	int8_t			s8;	/**< Signed 8 bit value			*/
-typedef	int16_t			s16;	/**< Signed 16 bit value		*/
-typedef	int32_t			s32;	/**< Signed 32 bit value		*/
-typedef	volatile u8		vu8;	/**< volatile Unsigned 8 bit value	*/
-typedef	volatile u16		vu16;	/**< volatile Unigned 16 bit value	*/
-typedef	volatile u32		vu32;	/**< volatile Unsigned 32 bit value	*/
-typedef	volatile s8		vs8;	/**< volatile Signed 8 bit value	*/
-typedef	volatile s16		vs16;	/**< volatile Signed 8 bit value	*/
-typedef	volatile s32		vs32;	/**< volatile Signed 8 bit value	*/
+typedef	uint8_t		u8;	/**< Unsigned 8 bit value	*/
+typedef	uint16_t	u16;	/**< Unsigned 16 bit value	*/
+typedef	uint32_t	u32;	/**< Unsigned 32 bit value	*/
+typedef	int8_t		s8;	/**< Signed 8 bit value	*/
+typedef	int16_t		s16;	/**< Signed 16 bit value	*/
+typedef	int32_t		s32;	/**< Signed 32 bit value	*/
+typedef	volatile u8	vu8;	/**< volatile Unsigned 8 bit value	*/
+typedef	volatile u16	vu16;	/**< volatile Unigned 16 bit value	*/
+typedef	volatile u32	vu32;	/**< volatile Unsigned 32 bit value	*/
+typedef	volatile s8	vs8;	/**< volatile Signed 8 bit value	*/
+typedef	volatile s16	vs16;	/**< volatile Signed 8 bit value	*/
+typedef	volatile s32	vs32;	/**< volatile Signed 8 bit value	*/
+
 
 //---------------------------------------------------------------------------------
 #endif // _gba_types_h_
